9fd7153a44879e4b95166327630709106f572d37
hadeel <hadeelelzansty92@gmail.com>
2023-01-27 22:52:53 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let barDiv = document.querySelector(".barDiv");
+let dropList = document.querySelector(".dropList");
+barDiv.addEventListener("click",toggleNav);
+function toggleNav(){
+    
+    dropList.classList.toggle("appDis")
+
+}
